cb47751be93e1459a67ddfdb1cb91c9fe892b247
kjown <106053989+kjown@users.noreply.github.com>
2023-08-11 16:59:51 +0800

    Update camel.py

--- pset2/camel.py
+++ pset2/camel.py
@@ -2,6 +2,7 @@ s = input("camelCase: ")
 print("snake_case: ", end="")
 for c in s:
     if c.isupper() == True:
-        print("_" + c.lower(), end="")
+        print("_" + c.lower(), end="", sep="")
     else:
-        print(c, end="" )
+        print(c, end="")
+print()
