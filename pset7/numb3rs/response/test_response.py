230f4ca3bf513c36ccde317f92c95751adb5e067
kjown <106053989+kjown@users.noreply.github.com>
2023-08-29 17:13:35 +0800

    Delete pset7/numb3rs directory

--- pset7/numb3rs/response/test_response.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from response import validate
-
-def test_validate():
-    assert validate("malan@harvard.edu") = True
-    assert validate("own@yahoo.com") = True
-    assert validate("malan@@@harvard.edu") = False
-    assert validate("own@yahoo..com") = False
